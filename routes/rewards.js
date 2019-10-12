7615b6f2759cbe1d6e2969a21cc42feffc6c39ac
SteliosPhanartzis <stelios.phan@gmail.com>
2019-10-12 07:51:37 -0400

    IT READS BARCODES

--- routes/rewards.js
+++ routes/rewards.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var mysql = require('mysql2');
+var multer = require('multer');
+var upload = multer({dest: __dirname + '/../public/images'});
+var jsBarcodeReader = require('javascript-barcode-reader')
 if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
 var router = express.Router();
 /* MySQL Set Up */
@@ -19,13 +22,33 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', upload.single('barcode'),async function(req, res, next) {
+	var code = 0;
 	var user = "testuser@gmail.com";
-	var query = "UPDATE user SET points=points+100 WHERE email='" + user + "'"
-	connection.query(query, (err, rows) => {
+	if(req.file) {
+		Image = req.file.path
+		try {
+		  code = await jsBarcodeReader(
+		    Image /* Image ID || HTML5 Image || HTML5 Canvas || HTML5 Canvas ImageData || Image URL */,
+		    {
+		      barcode: 'ean-13',
+		      type: 'industrial', //optional type
+		    }
+		  )
+		  console.log(code)
+			var query = "UPDATE user SET points=points+(SELECT retPoints from Item WHERE prodId =" + code + " ) WHERE email='" + user + "'"
+			connection.query(query, (err, rows) => {
+				if (err) throw err;
+			});
+		} catch (err) {
+		  console.log(err)
+		}
+	}
+	var qry = "SELECT points FROM user WHERE email = '" + user + "'";
+	connection.query(qry, (err, rows) => {
 		if (err) throw err;
+		res.json(rows[0]);
 	});
-	res.json(user);
 });
 function sanitizer(input) {
 	var reg = /[&<>"'/\\]/ig;
