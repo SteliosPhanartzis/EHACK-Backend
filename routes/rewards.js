caa58fe3d63e72ac5ca75d266862d6ab36e4d813
SteliosPhanartzis <stelios.phan@gmail.com>
2019-10-17 13:21:55 -0400

    commented full log

--- routes/rewards.js
+++ routes/rewards.js
@@ -36,7 +36,7 @@ router.post('/', upload.single('barcode'),async function(req, res, next) {
 		    }
 		  )
 		  console.log(code)
-		  console.log(req)
+		  // console.log(req)
 		  console.log(req.file)
 			var query = "UPDATE user SET points=points+(SELECT retPoints from Item WHERE prodId =" + code + " ) WHERE email='" + user + "'"
 			connection.query(query, (err, rows) => {
